f750dea25ffd671d71095c92f3ce3907bcbd792e
Vishesh Goyal <visheshgoyal2103@gmail.com>
2022-07-23 00:18:14 +0530

    updating home.js

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,15 +1,18 @@
-import { useAuthContext } from '../../hooks/useAuthContext';
-import { useCollection } from '../../hooks/useCollection';
+import { useAuthContext } from '../../hooks/useAuthContext'
+import { useCollection } from '../../hooks/useCollection'
+
 // styles
 import styles from './Home.module.css'
 
 // components
 import TransactionForm from './TransactionForm'
-import TransactionList from './TransactionList';
+import TransactionList from './TransactionList'
 
 export default function Home() {
-  const {user} = useAuthContext()
-  const {documents, error} = useCollection('transactions')
+  const { user } = useAuthContext()
+  const { documents, error } = useCollection(
+    'transactions', ["uid", "==", user.uid], ['createdAt', 'desc']
+  )
 
   return (
     <div className={styles.container}>
